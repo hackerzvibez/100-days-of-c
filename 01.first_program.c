b512cb7249906345d50015e222711ce8c29addb6
hackerzvibez <hackerzvibez@gmail.com>
2025-09-25 10:48:21 +0530

    Update 01.first_program.c

--- 01.first_program.c
+++ 01.first_program.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    printf(" This is KTASHWIN ... victory ,success , triumph");
+    printf(" This is KTASHWIN ... victory ,success , triumph"); // prints the string of characters inside a printf function to the default output
     return 0;
 }
