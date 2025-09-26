8a5dfe4e69afa4477e6fa2911a1969194269ad7a
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-26 15:01:21 +0530

    expanded the calculator program...

--- my_c_programs/02.calculator_program.c
+++ my_c_programs/02.calculator_program.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 // fucntion declarations
+void calculator();
 int stringlength(const char *v);
 int int_or_float(const char *v);
 int convert_to_int(const char *v);
 double convert_to_float(const char *v);
+float f_add(float a, float b);
+float f_mul(float a, float b);
+float f_div(float a, float b);
+float f_sub(float a, float b);
+int add(int a, int b);
+int mul(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
 
 int main()
 {
-    char *v = "123.5";
-    if (int_or_float(v))
-    {
-        printf("Int\n");
-    }
-    else
-    {
-        printf("Float\n");
-    }
-    printf("%.6f\n", convert_to_float("123.578945"));
+    calculator();
     return 0;
 }
 
@@ -76,7 +76,6 @@ double convert_to_float(const char *v)
         dot_len += 1;
         c = *(v + i + 1);
     }
-    printf("%d\n%d\n", len_of_string, dot_len);
     int e = len_of_string - dot_len - 1;
     switch (e)
     {
@@ -112,4 +111,116 @@ int stringlength(const char *v)
         c = *(v + i + 1);
     }
     return count;
+}
+
+float f_add(float a, float b)
+{
+    return a + b;
+}
+int add(int a, int b)
+{
+    return a + b;
+}
+float f_sub(float a, float b)
+{
+    return a - b;
+}
+int sub(int a, int b)
+{
+    return a - b;
+}
+float f_mul(float a, float b)
+{
+    return a * b;
+}
+int mul(int a, int b)
+{
+    return a * b;
+}
+float f_div(float a, float b)
+{
+    return a / b;
+}
+int div(int a, int b)
+{
+    return a / b;
+}
+
+void calculator()
+{
+    char op;
+    char a[100];
+    char b[100];
+    int i_a;
+    int i_b;
+    float f_a;
+    float f_b;
+    float (*func_f)(float, float);
+    int (*func_i)(int, int);
+
+    printf("Enter the Operation you want to perform (+,-,/,*) :");
+    scanf(" %c", &op);
+
+    printf("Enter the Number A :");
+    scanf(" %99s", a);
+
+    printf("Enter the Number B :");
+    scanf(" %99s", b);
+
+    if (int_or_float(a) && int_or_float(b))
+    {
+        printf("I am inside a if block\n");
+        i_a = convert_to_int(a);
+        i_b = convert_to_int(b);
+        switch (op)
+        {
+        case '+':
+            func_i = add;
+            break;
+        case '-':
+            func_i = sub;
+            break;
+        case '*':
+            func_i = mul;
+            break;
+        case '/':
+            if (i_b == 0)
+            {
+                printf("<Error!> division by zero");
+                return;
+            }
+            func_i = div;
+            break;
+        }
+        int result = func_i(i_a, i_b);
+        printf("Result : %d", result);
+    }
+    else
+    {
+        printf("i am inside else block\n");
+        f_a = convert_to_float(a);
+        f_b = convert_to_float(b);
+        switch (op)
+        {
+        case '+':
+            func_f = f_add;
+            break;
+        case '-':
+            func_f = f_sub;
+            break;
+        case '*':
+            func_f = f_mul;
+            break;
+        case '/':
+            if (f_b == 0.0)
+            {
+                printf("<Error! division by zero!>");
+                return;
+            }
+            func_f = f_div;
+            break;
+        }
+        float result = func_f(f_a, f_b);
+        printf("Result :%f", result);
+    }
 }
