02f8c9e655ebc2b64a4421dc940ee1a41bb889d0
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-28 14:45:17 +0530

    Tweaked some things

--- my_c_programs/04.Reverse_a_string.c
+++ my_c_programs/04.Reverse_a_string.c
@@ -4,16 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Global Vaiables
+
 // functions declarations
 size_t len(const char *v);
-void reverse_string_v(const char *v);
+char *reverse_string_v(const char *v);
+char *read_line();
 
 // main aka progrms entry point
 int main()
 {
-    const char *c = "Ktashwin2534@gmail.com ";
-    printf("%zu\n", len(c));
-    reverse_string_v(c);
+    printf("Enter the String you wanna reverse :");
+    char *in = read_line();
+    if (!in)
+        return 1;
+    char *reversed = reverse_string_v(in);
+    printf("Orginal :%s\nReversed :%s\n", in, reversed);
+    free(in);
+    free(reversed);
     return 0;
 }
 
@@ -28,14 +36,37 @@ size_t len(const char *v)
     return len;
 }
 
-void reverse_string_v(const char *v)
+char *reverse_string_v(const char *v)
 {
     size_t len_ = len(v);
-    char c[len_ + 1];
+    char *c = malloc(len_ + 1);
     for (size_t i = 0; i < len_; i++)
     {
         c[i] = v[len_ - i - 1];
     }
     c[len_] = '\0';
-    printf("Original :%s\nReversed :%s\n", v, c);
+    return c;
+}
+
+char *read_line()
+{
+    size_t size = 128;
+    size_t len_ = 0;
+    char *str = malloc(size);
+    if (!str)
+        return NULL;
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        str[len_++] = ch;
+        if (len_ == size)
+        {
+            size *= 2;
+            str = realloc(str, size);
+            if (!str)
+                return NULL;
+        }
+    }
+    str[len_] = '\0';
+    return str;
 }
